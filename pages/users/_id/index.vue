040f94cc2012cdfac606ae9ea6f2ba98e0801423
TheKaizenWolf <raul670saavedra@gmail.com>
2020-09-27 19:14:44 -0300

    starter files

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>A single user, with ID: {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
